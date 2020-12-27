fd88053dfc867226987ed0606d0eac11ce52907c
freemahn <gordon.pav@gmail.com>
2020-12-27 17:39:43 +0100

    fix: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ tasks.withType<Test> {
 // run docker login in case of errors
 jib {
     to {
-        image = "docker.pkg.github.com/pavelgordon/object-randomizer"
+        image = "docker.pkg.github.com/pavelgordon/object-randomizer-spring"
     }
 }
 
